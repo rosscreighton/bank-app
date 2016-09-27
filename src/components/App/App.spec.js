786b03aa2ffbad5ddcf19a6bbb2603e58ccf2132
rosscreighton <rosson.creighton@gmail.com>
2016-09-26 23:48:31 -0400

    removes unneeded App spec

--- src/components/App/App.spec.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import test from 'ava';
-import { spy } from 'sinon';
-import { mount } from 'enzyme';
-import { App } from './App';
-
-test.todo('<App /> tests needed?');
